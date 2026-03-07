8e2d58dcb9e845dc57e0662df8beee65e83f797c
Eric Alforque <eric.alforque@gmail.com>
2026-03-08 08:19:01 +1000

    Resolve dependency and dependabot conflicts

--- src/interfaces/model_fields.interface.ts
+++ src/interfaces/model_fields.interface.ts
@@ -1,7 +1,8 @@
 import { Model, ModelStatic } from "sequelize";
 
-export default interface ModelStaticWithFields<T extends Model>
-  extends ModelStatic<T> {
+export default interface ModelStaticWithFields<
+  T extends Model,
+> extends ModelStatic<T> {
   DEFAULT_FIELDS: string[];
   SELECTABLE_FIELDS: string[];
 }
